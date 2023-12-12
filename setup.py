4914ada47c330fc2eccea680cc9d89673be09d64
Bintang Pradana Erlangga Putra <bintang.pradana@privyid.tech>
2023-12-12 15:39:41 +0700

    fix: sub package not included in build

--- setup.py
+++ setup.py
@@ -2,12 +2,16 @@ import os
 
 from setuptools import setup, find_packages
 
-VERSION = "0.0.2"
+VERSION = "0.0.3"
 DESCRIPTION = "A Python library for streaming video over RTSP"
 
 
-def read(fname):
+def long_description(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+def requirements(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read().splitlines()
 
 
 setup(
@@ -16,11 +20,11 @@ setup(
     author="Bintang Pradana Erlangga Putra",
     author_email="<work.bpradana@gmail.com>",
     description=DESCRIPTION,
-    long_description=read("README.md"),
+    long_description=long_description("README.md"),
     long_description_content_type="text/markdown",
     url="https://github.com/bpradana/zephyr",
     packages=find_packages(),
-    install_requires=["opencv-python", "numpy", "tenacity"],
+    install_requires=requirements("requirements.txt"),
     keywords=["python", "rtsp", "streaming", "video"],
     classifiers=[
         "Development Status :: 1 - Planning",
